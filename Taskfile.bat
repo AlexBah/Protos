a684b0780760487dbb344522c1804c2232b95d0c
Alex Bah <P_osts@mail.ru>
2025-09-07 20:44:48 +0700

    refactor big version

--- Taskfile.bat
+++ Taskfile.bat
@@ -4,6 +4,6 @@ REM         description: "Generate Go code from proto files"
 REM         cmd:
 protoc -I proto proto\sso\sso.proto --go_out=gen\go\ --go_opt=paths=source_relative --go-grpc_out=gen\go\ --go-grpc_opt=paths=source_relative
 
-REM         description: "Generate JS code from proto files"
+REM         description: "Generate grpc-gateway code from proto files"
 REM         cmd:
-protoc -I proto proto\sso\sso.proto --js_out=import_style=commonjs,binary:gen\js\ --plugin=protoc-gen-js=..\protobuf-javascript-3.21.4-win64\bin\protoc-gen-js.exe --grpc-web_out=import_style=commonjs,mode=grpcwebtext:gen\js\
+protoc -I proto proto\sso\sso.proto --grpc-gateway_out=gen\go\ --grpc-gateway_opt=paths=source_relative --grpc-gateway_opt=generate_unbound_methods=true
